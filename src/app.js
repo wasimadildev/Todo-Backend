7c6b094e36441194b935227d0dfd59f878fd61fd
Wasim Adil <waseemadilcode@gmail.com>
2025-03-13 01:12:17 +0500

    routes configure login & register routes test also

--- src/app.js
+++ src/app.js
@@ -2,12 +2,24 @@ const express = require("express");
 const connectDB = require("./config/database.js");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/userRoutes.js");
+const errorHandler = require("./utils/errorHandler.js");
 const app = express();
 dotenv.config();
-app.use(express.json());
-app.use(cookieParser());
+
 const PORT = process.eventNames.PORT || 3000;
 
 connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/api/users" , userRoutes);
+
+
+app.use(errorHandler)
+
+
 
 app.listen(PORT, () => console.log(`Server runing on port ${PORT}`));
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require("../models/user.js")
 const {validateRegister, validateLogin} = require("../utils/validators.js")
-const validateLogin = require("../utils/validators.js")
+const jwt = require("jsonwebtoken")
 const bcrypt = require("bcrypt")
 const registerUser = async(req , res , next) =>{
     try {
@@ -39,23 +39,29 @@ const loginUser = async (req, res, next) =>{
         validateLogin(req);
         // Check Email already Exist 
         const {emailId , password} = req.body;
+        console.log(emailId , password)
         const user = await User.findOne({emailId});
+        console.log(user.firstName)
         if(!user){
             return res.status(404).json({error: "User Not Found"})
         }
         // compare password
         const isMatch = await bcrypt.compare(password , user.password);
+       
         if(!isMatch){
             return res.status(401).json({error: "Invalid Password"})
         }
 
         // Genrate JWT Token
 
-        const token = await isJWT.sign({_id: user._id} , process.env.JWT_SECRET, process.env.JWT_EXPIRY); 
+        const token = await jwt.sign({_id: user._id}, "secret"); 
 
         // send token in cookies 
-        res.cookie("token" , token)
+        console.log(token)
+        res.cookie("token", token)
+
         // send res message and token
+
         res.send("Login Successfully")
         
     } catch (error) {
@@ -65,4 +71,4 @@ const loginUser = async (req, res, next) =>{
     }
 }
 
-exports.module = {registerUser , loginUser}
+module.exports = { registerUser, loginUser };
